9fcfcc3e318675b6ac2aa568e57d61c91324c58a
KingDada <joerg.lautenschlager@gmail.com>
2014-05-16 20:44:49 +0200

    outsorced rating function in a separate class

--- wp-content/themes/myMovies/includes/ratings-service.php
+++ wp-content/themes/myMovies/includes/ratings-service.php
@@ -1,87 +1,77 @@
 <?php
 
-/**
- * Add bookmark class
- * + int count;
- * + bool empty;
- * + delete option function(name);
- * + create option function(name);
- * + isBookmarked function(id);
- * + add bookmark function(id);
- * + remove bookmark function(id); 
- */
-
-/**
- * Add ajax bookmark hook and handle requests
- */
 class Rating {
-    
+
     static $user;
+
     function __construct() {
         $this->user = wp_get_current_user();
     }
+
     function get_public_movie_rating($movie_id) {
         $all_ratings = $GLOBALS['wpdb']->get_results(''
-            . 'SELECT * '
-            . 'FROM wp_ratings '
-            . 'WHERE movie = '.$movie_id, OBJECT);
-        
+                . 'SELECT * '
+                . 'FROM wp_ratings '
+                . 'WHERE movie = ' . $movie_id, OBJECT);
+
         $votes = 0;
         $sum_ratings = 0;
-        if(!empty($all_ratings)) {
-            foreach($all_ratings as $rate) {
+        if (!empty($all_ratings)) {
+            foreach ($all_ratings as $rate) {
                 ++$votes;
                 $sum_ratings += $rate->rating;
             }
         }
-        $global_rating = $votes > 0 ? round($sum_ratings/$votes, 1) : '';
-        
+        $global_rating = $votes > 0 ? round($sum_ratings / $votes, 1) : '';
+
         return $global_rating;
     }
-    
+
     function get_user_movie_rating($movie_id) {
         $user_ratings = $GLOBALS['wpdb']->get_results(''
-            . 'SELECT * '
-            . 'FROM wp_ratings '
-            . 'WHERE movie = '.$movie_id
-                . ' AND user = '.$this->user->ID, OBJECT);
-        
-        if(!empty($user_ratings) && count($user_ratings == 1)) {
+                . 'SELECT * '
+                . 'FROM wp_ratings '
+                . 'WHERE movie = ' . $movie_id
+                . ' AND user = ' . $this->user->ID, OBJECT);
+
+        if (!empty($user_ratings) && count($user_ratings == 1)) {
             $rating = $user_ratings[0]->rating;
         }
-        
+
         return $rating;
     }
-    
+
     function set_user_movie_rating($movie_id, $rating) {
         $data = array(
             'user' => $this->user->ID,
             'movie' => $movie_id,
             'rating' => $rating
         );
-        
-        $alreadyVoted = $GLOBALS['wpdb']->get_results(''
-            . 'SELECT * '
-            . 'FROM wp_ratings '
-            . 'WHERE user = '.$this->user->ID
-            . ' AND movie = '.$movie_id);
 
-        if(empty($alreadyVoted)) {
+        $alreadyVoted = $GLOBALS['wpdb']->get_results(''
+                . 'SELECT * '
+                . 'FROM wp_ratings '
+                . 'WHERE user = ' . $this->user->ID
+                . ' AND movie = ' . $movie_id);
+
+        if (empty($alreadyVoted)) {
             $GLOBALS['wpdb']->insert('wp_ratings', $data);
         } else {
             $GLOBALS['wpdb']->update('wp_ratings', $data, array('user' => $this->user->ID, 'movie' => $movie_id));
         }
     }
+
 }
 
-
+/**
+ * Add ajax bookmark hook and handle requests
+ */
 add_filter('wp_ajax_mm_rating', 'mm_rating');
-        
-function mm_rating()
-{
+
+function mm_rating() {
     $Rate = new Rating();
     $data = $_REQUEST;
-    
+
     $Rate->set_user_movie_rating($data['movie'], $data['rating']);
     $global_rating = $Rate->get_public_movie_rating($data['movie']);
 
@@ -92,7 +82,7 @@ function mm_rating()
         'global_rating' => $global_rating,
         'rating' => $data['rating']
     );
-    
+
     echo json_encode($returnArray);
     die();
 }
--- wp-content/themes/myMovies/movie-template.php
+++ wp-content/themes/myMovies/movie-template.php
@@ -11,42 +11,42 @@ $userloggedin = is_user_logged_in() ? true : false;
 $Rate = new Rating();
 $global_rating = $Rate->get_public_movie_rating($post->ID);
 $user_rating = $Rate->get_user_movie_rating($post->ID);
-
 ?>
 <div class="row movie padding-top-15">
     <div class="col-md-3 col-sm-4 col-xs-5">
-        <?php
-        if (isset($movieimagepath))
-            echo "<img src='$movieimagepath' class='img-responsive'>";
-        ?>
+<?php
+if (isset($movieimagepath))
+    echo "<img src='$movieimagepath' class='img-responsive'>";
+?>
     </div>
     <div class="col-md-9 col-sm-8 col-xs-7">
         <div class="row border-bottom-white padding-bottom-15">
             <div class="col-sm-7">
-                <?php
-                if (isset($movietitle))
-                    echo "<h2 class='color-primary'>$movietitle</h2>";
+<?php
+if (isset($movietitle))
+    echo "<h2 class='color-primary'>$movietitle</h2>";
 
-                if (isset($moviesubtitle))
-                    echo "<h3>$moviesubtitle</h3>";
+if (isset($moviesubtitle))
+    echo "<h3>$moviesubtitle</h3>";
 
-                echo '<span class="small">';
-                if (isset($moviestudio))
-                    echo $moviestudio;
+echo '<span class="small">';
+if (isset($moviestudio))
+    echo $moviestudio;
 
-                if (isset($movieyear))
-                    echo ', ' . $movieyear;
+if (isset($movieyear))
+    echo ', ' . $movieyear;
 
-                echo '</span>';
-                ?>
+echo '</span>';
+?>
             </div>
             <div class="col-md-5 col-sm-12 movie-addon-block">
                 <div class="row">
                     <div class="rating col-md-12 col-sm-6 padding-bottom-15">
                         Rating<br>
                         <span class="star stars-empty movie-<?php echo $post->ID ?>"><?php echo $global_rating ?></span>
-                        <span class="rating-stars" is-user="<?php if($userloggedin) echo false; else echo true; ?>" movie-id="<?php echo $post->ID; ?>" data-score="<?php echo $user_rating; ?>"></span>
-                            
+                        <span class="rating-stars" is-user="<?php if ($userloggedin) echo false;
+                else echo true; ?>" movie-id="<?php echo $post->ID; ?>" data-score="<?php echo $user_rating; ?>"></span>
+
                     </div>
                     <div class="buttons col-md-12 col-sm-6">
 
@@ -66,7 +66,8 @@ $user_rating = $Rate->get_user_movie_rating($post->ID);
                         }
                         ?>
 
-                        <button class="btn <?php if($userloggedin) echo 'btn-info'; else echo 'btn-disabled'; ?>">+ Leihen</button>
+                        <button class="btn <?php if ($userloggedin) echo 'btn-info';
+                        else echo 'btn-disabled'; ?>">+ Leihen</button>
 
                     </div>
                 </div>
