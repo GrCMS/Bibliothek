136145f985290e5b8e1a1d6f650172716a7fc7f5
Pabzt <d.meier@mailbox.org>
2014-05-20 17:37:00 +0200

    removed template-all-movies-page, modified archive-movies.php and added a menu itm in order to show all posts from custom post type 'movies' (using wordpress template hierarchy)

--- wp-content/themes/myMovies/archive-movies.php
+++ wp-content/themes/myMovies/archive-movies.php
@@ -4,7 +4,19 @@
  * Template called for custom post type 'movies'
  */
 
+get_header();
 
+?>
+
+<div class="container">
+    
+    <h1>ALL MOVIES TEMPLATE</h1>
+    
+</div>
+
+<?php 
+
+get_footer();
 
 ?>
 
