02b666b8bce719b948f630e73e05774b1e71af86
Pabzt <d.meier@mailbox.org>
2014-05-20 17:43:20 +0200

    added some comments

--- wp-content/themes/myMovies/includes/custom-menu-items.php
+++ wp-content/themes/myMovies/includes/custom-menu-items.php
@@ -1,7 +1,14 @@
 <?php
 
+/**
+ * Hook called on 'wp_nav_menu_items' in order to render a link to the custom post type archive for 'movies'
+ */
 add_filter('wp_nav_menu_items', 'add_cpt_movies_archive_link', 10, 2);
 
+/**
+ * Adds the 'All Movies' link to the genre slider (archive-movies.php)
+ * ATTENTION: This is just a workaround. The link will not show up in the backend.
+ */
 function add_cpt_movies_archive_link($items, $args) {
     if ($args->theme_location == 'slider') {
         $movies_archive_link = get_post_type_archive_link( 'movies' );
@@ -11,8 +18,14 @@ function add_cpt_movies_archive_link($items, $args) {
     return $items;
 }
 
+/**
+ * Hook called on 'wp_nav_menu_items' in order to render a logout and backend link
+ */
 add_filter('wp_nav_menu_items', 'add_logout_link', 10, 2);
 
+/**
+ * Adds the 'logout' and 'backend' link to the account menu
+ */
 function add_logout_link($items, $args) {
     if ($args->theme_location == 'account') {
         $loginoutlink = wp_loginout('index.php', false);
