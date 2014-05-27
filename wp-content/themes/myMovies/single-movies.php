aa0ae94e71fdfb9099d562dc7de481ae3e96c4e4
Pabzt <d.meier@mailbox.org>
2014-05-27 17:18:59 +0200

    added comments to single-movie.php

--- wp-content/themes/myMovies/single-movies.php
+++ wp-content/themes/myMovies/single-movies.php
@@ -1,9 +1,30 @@
-<?php get_header();
-	
+<?php 
 
-	the_post();
-	require("templates/template-movie.php"); 
+    get_header();
+    
+    the_post();
+    require("templates/template-movie.php"); 
+    
+    
+    $args = array(
+        
+        'post_id' => get_the_ID()
+    );
+    
+    $comments = get_comments( $args );
 
+    echo "<div class='container'>";
 
- get_footer(); 
+    foreach($comments as $comment)
+    {
+        echo "<article>";
+        echo "<h5>". $comment->comment_author ."</h5>";
+        echo "<div>" .$comment->comment_content . "</div>";
+        echo "</article>";
+    }
+
+    echo "</div>";
+ 
+    get_footer(); 
+
  ?>
