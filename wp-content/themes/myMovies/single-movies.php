3e54ab9bb2c26f091c162c5b5fecd1c3b9bda2ca
Pabzt <d.meier@mailbox.org>
2014-06-05 18:18:13 +0200

    added comments with rating from plugin in single-movies.php

--- wp-content/themes/myMovies/single-movies.php
+++ wp-content/themes/myMovies/single-movies.php
@@ -3,7 +3,7 @@
     get_header();
     
     the_post();
-    require("templates/template-movie.php"); 
+    get_template_part("templates/template","movie"); 
     
     $args = array(
         
@@ -11,14 +11,71 @@
     );
     
     $comments = get_comments( $args );
-
-    echo "<div class='container'>";
+    
+    include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+    $show_rating = is_plugin_active('mm-comment-rating/comment-rating.php');
+        
+    if($show_rating) {
+                
+        $current_post_comments = new movie_comments();
+        echo "<h3 class='color-primary container text-right'>Bewertungen (" . $current_post_comments->getCommentCount(get_the_ID()) . ")</h3>";
+        
+    } else {
+        
+        echo "<h3 class='color-primary container text-right'>Bewertungen</h3>";
+    }
+    
+    echo "<div class='movie-divider'>";
+    echo "<div class='container'>";   
 
     foreach($comments as $comment) {
+                        
         if($comment->comment_content) {
-            echo "<article>";
-            echo "<h5>". $comment->comment_author ."</h5>";
-            echo "<div>" .$comment->comment_content . "</div>";
+            
+            echo "<article class='mm-comments-style'>";
+            echo "<div class='row'>";
+            echo "<div class='col-xs-8'><h5 class='color-primary'> Bewertung von: ". $comment->comment_author ."</h5></div>";
+            echo "<div class='col-xs-4'><h5 class='rating text-right'>"; 
+            
+            ?>    
+            
+                <?php 
+                
+                if($show_rating): 
+                
+                    $rating = $current_post_comments->getUserRating($comment->comment_post_ID, $comment->comment_ID);
+                    
+                ?>
+                    
+                <span class="hidden ratingvalue"><?php echo $rating; ?></span>
+                <ul class="color-primary">
+                    <?php
+                    for ($i = 1; $i < 6; $i++) {
+                        // Setzen der Ratingklassen
+                        $starvalue;
+                        if ($rating >= $i)
+                            $starvalue = "filled";
+
+                        else if ($rating > ($i - 0.7))
+                            $starvalue = "half";
+                        else
+                            $starvalue = "empty";
+
+                        echo('<li class="star star1 stars-' . $starvalue . '"></li>');
+                    }
+                    ?>
+
+                </ul>
+                
+                <?php endif; ?>
+                            
+            <?php 
+            
+            echo "</h5></div>";
+            echo "</div>";
+            echo "<div class='row'>";
+            echo "<div class='col-xs-12'>" .$comment->comment_content . "</div>";
+            echo "</div>";
             echo "</article>";
         }
     }
